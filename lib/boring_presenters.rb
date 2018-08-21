3c995bb318fed2346de79262c57a109e75d1cd5e
Wyatt Kirby <wyatt@apsis.io>
2018-08-21 09:23:15 -0700

    Include translation helpers for Rails

--- lib/boring_presenters.rb
+++ lib/boring_presenters.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 require "boring_presenters/version"
 require "boring_presenters/presenter"
+
+require "boring_presenters/rails" if defined? Rails
---
+++ lib/boring_presenters/rails.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Boring
+  class Presenter
+    include AbstractController::Translation
+  end
+end
